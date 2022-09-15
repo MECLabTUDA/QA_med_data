df86da0c0d6d614aed04de886caf99dfe0079be1
Amin Ranem <aminranem@googlemail.com>
2022-09-15 13:24:06 +0200

    Added empty branch for MRI QA.

--- test/eval/test_accumulator.py
+++
@@ -1,8 +0,0 @@
-from mp.eval.accumulator import Accumulator
-
-def test_acc():
-    acc = Accumulator(keys=['A'])
-    for i in range(5):
-        acc.add('A', float(i))
-    assert acc.mean('A') == 2.0
-    assert 1.41 < acc.std('A') < 1.415
--- test/utils/test_introspection.py
+++
@@ -1,6 +0,0 @@
-from mp.utils.introspection import introspect
-
-def test_introspection():
-    class_path = 'mp.models.classification.small_cnn.SmallCNN'
-    exp = introspect(class_path)()
-    assert exp.__class__.__name__ == 'SmallCNN'
